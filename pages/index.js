723b92e75bf7833dc5854e844e9501c92d9be9e3
Syed Ashraf <syedashraf.123.123@gmail.com>
2022-04-18 00:47:18 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+export default function Home() {
+  // const [user, setUser] = useState(res[0] ? res[0] : {});
+  const user = {};
+  return (
+    <>
+      <h1>
+        {user ? (
+          <>
+            {user?.name} {user?.age}
+          </>
+        ) : (
+          "Loading..."
+        )}
+      </h1>
+    </>
+  );
+}
+
+// export async function getServerSideProps() {
+// const response = await fetch("http://127.0.0.1:5000");
+// const res = await response.json();
+// return { props: { res } };
+// }
